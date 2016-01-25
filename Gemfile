e62608071c1b7cf6895847924a2b85e9ed148616
Chris Zuidema <cwazuidema@gmail.com>
2016-01-25 20:39:02 +0200

    version 0.5.0

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+ruby "2.2.2"
+
+gem 'colorize'
+gem 'spreadsheet'
+gem 'highline'
---
+++ spreadsheet.rb
@@ -0,0 +1,48 @@
+require "spreadsheet"
+require "HighLine"
+require "colorize"
+
+@cli = HighLine.new
+
+def find_all_xls_files
+# find all xls files in the current folder		
+	puts "Yoyo, I've found these files bro:".blue
+	books = Dir['*.xls']
+	books.each do |book|
+		puts book.red
+	end
+	open_xls
+end
+
+def open_xls
+	book = Spreadsheet.open @cli.ask "Which book do you want to open? (use full filename inc .xls)".blue
+	sheet1 = book.worksheet 0
+	options_for_book(book, sheet1)
+end
+
+def options_for_book(book, sheet1)
+	answer = @cli.ask "Type 'headers' to view the headers or 'client' to enter a new order".blue
+  see_headers(sheet1) if answer == 'headers'
+  add_client(book, sheet1) if answer == 'client'
+end
+
+def see_headers(sheet1)
+	header = sheet1.row(0)
+  header.each do |header|
+ 	puts header.red
+  end
+end
+
+def add_client(book, sheet1)
+	counter = 0
+	header = sheet1.row(0)
+	header.each do |header|
+		sheet1.rows[1][counter] = @cli.ask "What data do we need for #{ header.red }"
+    counter = counter +1
+  end
+  book.write @cli.ask "What is the filename of this file (add .xls at the end)"
+end
+
+
+
+find_all_xls_files
